7f1a6c6e1b60465198d1703d6eab5b91fc799d8d
Ayush Goyal <ayush987goyal@gmail.com>
2018-10-24 23:43:25 +0530

    Add basic routing with form

--- basics/app.js
+++ basics/app.js
@@ -1,7 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html></html>');
+    res.write('<head>Enter Message</head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    return res.end();
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html><h1>Hello from node</h1></html>');
+  res.end();
 });
 
 server.listen(3000);
