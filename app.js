df7267304f6bbe7dd6c60adda560f92788bb3e06
Ayush Goyal <ayush987goyal@gmail.com>
2018-10-25 19:38:40 +0530

    Add body parser and form with post

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/add-product', (req, res, next) => {
-  console.log('In another middle');
-  res.send({ yoadd: 'fsdfsfsdfsf' });
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>'
+  );
+});
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In another middle');
   res.send({ yo: 'fsdfsf' });
 });
 
