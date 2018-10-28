9a1e549b42ce0092b6c7bfd5cbd20dc41b91ba17
Ayush Goyal <ayush987goyal@gmail.com>
2018-10-28 15:25:59 +0530

    Add relationship and create dummy user and add to req

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequlize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -17,14 +19,37 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequlize
+  // .sync({ force: true })
   .sync()
   .then(result => {
+    return User.findById(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'test', email: 'test@test.com' });
+    }
+
+    return user;
+  })
+  .then(() => {
     app.listen('3000');
   })
   .catch(console.log);
---
+++ models/user.js
@@ -0,0 +1,16 @@
+const Sequlize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const User = sequelize.define('user', {
+  id: {
+    type: Sequlize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: Sequlize.STRING,
+  email: Sequlize.STRING
+});
+
+module.exports = User;
