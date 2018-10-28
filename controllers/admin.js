7b30bdecbe40b155db4f15b259f5a9b38b954d97
Ayush Goyal <ayush987goyal@gmail.com>
2018-10-28 10:35:16 +0530

    De;ete product func

--- controllers/admin.js
+++ controllers/admin.js
@@ -51,3 +51,9 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
+};
