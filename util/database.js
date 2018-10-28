12aae55f7388abd60e45579ea281970c3df0be74
Ayush Goyal <ayush987goyal@gmail.com>
2018-10-28 14:41:43 +0530

    Initialize sequlize and define prod model

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequalize = require('sequelize');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: 'root'
+const sequelize = new Sequalize('node-complete', 'root', 'root', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
