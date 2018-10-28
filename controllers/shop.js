948581d1243714842f80105b1c318f17ce88cc95
Ayush Goyal <ayush987goyal@gmail.com>
2018-10-28 19:01:44 +0530

    Add cart resetting and orders output

--- controllers/shop.js
+++ controllers/shop.js
@@ -114,9 +114,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then(cart => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then(products => {
@@ -133,15 +135,19 @@ exports.postOrder = (req, res, next) => {
         .catch(err => console.log(err));
     })
     .then(() => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(() => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', { path: '/orders', pageTitle: 'Your Orders' });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', { path: '/checkout', pageTitle: 'Checkout' });
+  req.user
+    .getOrders({ include: ['products'] })
+    .then(orders => {
+      res.render('shop/orders', { path: '/orders', pageTitle: 'Your Orders', orders });
+    })
+    .catch(err => console.log(err));
 };
