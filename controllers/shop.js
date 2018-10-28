251cbb61c0816a9dcc034021dc89bc81af41621c
Ayush Goyal <ayush987goyal@gmail.com>
2018-10-28 12:36:49 +0530

    Add func for save and find by d

--- controllers/shop.js
+++ controllers/shop.js
@@ -15,9 +15,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', { pageTitle: product.title, path: '/products', product });
-  });
+  Product.findById(prodId)
+    .then(([product]) => {
+      res.render('shop/product-detail', {
+        pageTitle: product[0].title,
+        path: '/products',
+        product: product[0]
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
